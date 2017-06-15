645cd0b5d6d00ef07ae52cf7665b8717f61ed1fb
AdhikariSaurabh <saurabhadhikari1992@gmail.com>
2017-06-15 17:53:06 +0800

    new file:   SpMV/sparse_mxp.c 	new file:   SpMV/sparse_normal.c 	new file:   speedup_analysis/speedupbyte.pdf 	new file:   speedup_analysis/speedupbyte.png 	new file:   speedup_analysis/speeduphalf.pdf 	new file:   speedup_analysis/speeduphalfword.png 	new file:   speedup_analysis/speedupword.pdf 	new file:   speedup_analysis/speedupword.png 	deleted:    sparse_mxp.c 	deleted:    sparse_normal.c

---
+++ SpMV/sparse_mxp.c
@@ -0,0 +1,131 @@
+ #include<stdio.h>
+ #include<stdlib.h>
+ #include<time.h>                                                            
+ #include <string.h>
+ #include <sys/types.h> 
+ #include <sys/time.h>
+ #include <sys/resource.h>                                                   
+ #include <fcntl.h>
+ #include <unistd.h>
+ #include <inttypes.h>                                                       
+ #include <errno.h>
+ #include <math.h>                                                           
+ #include "vbx.h"                                                            
+ #include "vbx_port.h"  
+ #include "vbx_common.h"
+ #include "vbx_test.h"
+ #include "vbx_types.h"
+ #include "vectorblox_mxp_lin.h"
+ #define MXP 
+ #define M 6
+ int main()
+  {                                                                           
+   
+  #ifdef MXP 
+       VectorBlox_MXP_Initialize("mxp0","cma");
+  #else
+       printf("MXP Disabled, APP is running entirely on ARM\n");
+   #endif
+   vbx_mxp_print_params();
+   vbx_timestamp_t time_start,time_stop;
+  
+   // Assume 8x8 sparse matrix
+      int32_t sparseMatrix[8][8] =
+      {
+          {0 , 0 , 3 , 0 , 4,0,0,0 },
+          {0 , 0 , 5 , 7 , 0,0,0,0 },
+          {0 , 0 , 0 , 0 , 0,0,0,0 },
+          {0 , 2 , 6 , 0 , 0,0,0,0 },
+          {0 , 0 , 0 , 0 , 0,0,0,0 },
+          {0 , 0 , 0 , 0 , 0,0,0,0 },
+          {0 , 0 , 0 , 0 , 0,0,0,0 },
+          {0 , 0 , 0 , 0 , 0,0,0,0 }
+      };
+      int32_t i=0,j=0,k=0;
+      int32_t a;
+      double seconds;
+      int32_t size = 0;
+      int32_t vect[8]={1,2,3,4,5,6,7,8};
+      int32_t *value = vbx_shared_malloc(sizeof(int)*M);
+      int32_t *row_ptr = vbx_shared_malloc(sizeof(int)*M);
+      int32_t *column= vbx_shared_malloc(sizeof(int)*M);
+      int32_t *temp= vbx_shared_malloc(sizeof(int)*M);
+      int32_t resultvector[M];
+      vbx_word_t *v_vect = vbx_sp_malloc(M*sizeof(vbx_word_t));
+      vbx_word_t *v_value = vbx_sp_malloc(M*sizeof(vbx_word_t));
+      vbx_word_t *v_row_ptr = vbx_sp_malloc(M*sizeof(vbx_word_t));
+      vbx_word_t *v_column = vbx_sp_malloc(M*sizeof(vbx_word_t));
+      vbx_word_t *vtemp = vbx_sp_malloc(M*sizeof(vbx_word_t));
+      vbx_dcache_flush_all();
+  
+      vbx_set_vl(M);
+      vbx_timestamp_start();
+      time_start = vbx_timestamp();
+  
+  
+    // number of columns in compactMatrix (size) must be
+     // equal to number of non - zero elements in sparse matrix
+  
+     for (i = 0; i < 8; i++)
+          for (j = 0; j < 8; j++)
+              if (sparseMatrix[i][j] != 0)
+                  size++;
+  
+  
+      int csrMatrix[3][size];
+  
+      // Making of new matrix
+      for (i = 0; i < 8; i++)
+          for (j = 0; j < 8; j++)
+              if (sparseMatrix[i][j] != 0)
+              {
+                  csrMatrix[0][k] = i;
+                  csrMatrix[1][k] = j;
+                  csrMatrix[2][k] = sparseMatrix[i][j];
+                  k++;
+              }
+ 
+  
+     for (j = 0; j < 6; j++)
+       {
+         value[j]=csrMatrix[2][j];
+  
+         column[j]=csrMatrix[1][j];
+  
+         row_ptr[j]=csrMatrix[0][j];
+       }
+  
+  
+   vbx_dma_to_vector(v_column,column,M*sizeof(vbx_word_t));
+
+ 
+    for (j = 0; j < 6; j++)
+      {
+        temp[j] = vect[column[j]];
+      }
+ 
+
+   vbx_dma_to_vector(vtemp,temp,M*sizeof(vbx_word_t));
+   vbx_dma_to_vector(v_value,value,M*sizeof(vbx_word_t));
+ 
+   vbx( VVW,VMUL,vtemp,v_value,vtemp);
+   vbx_dma_to_host(value,vtemp,M*sizeof(vbx_word_t));
+   time_stop = vbx_timestamp();
+   seconds=vbx_print_scalar_time( time_start, time_stop );
+   printf("Took timer ticks -> %g s\n" , seconds);
+ 
+     for (i=0; i<3; i++)
+     {
+         for (j=0; j<size; j++)
+             printf("%d ",csrMatrix[i][j]);
+ 
+         printf("\n");
+     }
+ 
+     for(i=0;i<6;i++){
+ 
+             printf("%d \n",value[i]);
+          }
+ 
+     return 0;
+ }
---
+++ SpMV/sparse_normal.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h> 
+int main()
+{
+    // Assume 6x6 sparse matrix
+    int sparseMatrix[6][6] =
+    {
+        {0 , 0 , 3 , 0 , 4,0},
+        {0 , 0 , 5 , 7 , 0,0},
+        {0 , 0 , 0 , 0 , 0,0},
+        {0 , 2 , 6 , 0 , 0,0},
+        {0 , 0 , 0 , 0 , 0,0},
+        {0 , 0 , 0 , 0 , 0,0}
+        
+    };
+    int i=0,j=0,k=0;
+    int a;
+    int size = 0;
+    clock_t start,diff;
+    double milliseconds;
+    int vect[8]={1,2,3,4,5,6,7,8};
+    int *value = malloc(sizeof(int)*6);
+    int *row_ptr = malloc(sizeof(int)*6);
+    int *column=malloc(sizeof(int)*6);
+    int resultvector[6];
+   // number of columns in compactMatrix (size) must be
+   // equal to number of non - zero elements in sparse matrix
+ 
+ start = clock();
+   for (i = 0; i < 6; i++)
+        for (j = 0; j < 6; j++)
+            if (sparseMatrix[i][j] != 0)
+                size++;
+ 
+ 
+    int csrMatrix[3][size];
+ 
+    // Making of new matrix
+    for (i = 0; i < 6; i++)
+        for (j = 0; j < 6; j++)
+            if (sparseMatrix[i][j] != 0)
+            {
+                csrMatrix[0][k] = i;
+                csrMatrix[1][k] = j;
+                csrMatrix[2][k] = sparseMatrix[i][j];
+                k++;
+            }
+
+    for (j = 0; j < 6; j++)
+     {
+       value[j]=csrMatrix[2][j];
+     
+       column[j]=csrMatrix[1][j];
+     
+       row_ptr[j]=csrMatrix[0][j];
+     }
+
+
+
+    for (j = 0; j < 6; j++)
+     {
+       value[j] = value[j] * vect[column[j]];
+     }
+
+
+     diff = clock() - start;
+     milliseconds = (double) (diff) / CLOCKS_PER_SEC;
+/*    
+    for (j = 0; j < 6; j++)
+     {
+       value[j] = value[j] * vect[column[j]];
+     }
+*/
+
+
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<size; j++)
+            printf("%d ",csrMatrix[i][j]);
+ 
+        printf("\n");
+    }
+    
+    for(i=0;i<6;i++){
+    
+            printf("%d \n",value[i]);
+         }
+ 
+   
+
+   printf("Timer took -----> %f \n", milliseconds); 
+    return 0;
+}
+
